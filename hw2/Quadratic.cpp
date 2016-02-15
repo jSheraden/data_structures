2dc7fbdd73e8b356652069cc196efff53c86a2bc
jsheraden <josasheraden@gmail.com>
2016-02-15 00:33:50 +0000

    Added get and set functions to hw2/Quadratic.cpp

--- hw2/Quadratic.cpp
+++ hw2/Quadratic.cpp
@@ -1,6 +1,36 @@
 #include "Quadratic.hpp"
 
+// The default constructor initializes all values to 0.
 Quadratic::Quadratic()
 {
-
+    a = 0;
+    b = 0;
+    c = 0;
 }
+
+// Quadratic.set() - Assign new values to a, b, and c.
+void Quadratic::set( double newA, double newB, double newC )
+{
+    a = newA;
+    b = newB;
+    c = newC;
+}
+
+// Quadratic.getA() - Return the value of a.
+double Quadratic::getA()
+{
+    return a;
+}
+
+// Quadratic.getB() - Return the value of b.
+double Quadratic::getB()
+{
+    return b;
+}
+
+// Quadratic.getC() - Return the value of c.
+double Quadratic::getC()
+{
+    return c;
+}
+
