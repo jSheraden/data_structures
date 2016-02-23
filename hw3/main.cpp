fb0a63c04661fdf44da460430ef9131c923e598f
jsheraden <josasheraden@gmail.com>
2016-02-23 18:13:01 +0000

    Added assignment header to hw3/main.cpp

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,3 +1,9 @@
+// Name: Joseph Sheraden
+// Project Number: 3
+// Project Description: List Class with a Dynamic Array
+// Course: Data Structures
+// Instructor: Bob Comer
+// Date: February 23, 2016
 #include <iostream>
 #include "DList.cpp"
 
