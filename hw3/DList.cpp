6370c401910c9680aad8685cdefea9b9909a564b
jSheraden <josasheraden@gmail.com>
2016-02-26 17:39:30 -0600

    Added member functions to hw3/DList.cpp

--- hw3/DList.cpp
+++ hw3/DList.cpp
@@ -6,3 +6,39 @@ DList::DList()
     mySize = 0;
     std::cout << "Testing..." << std::endl;
 }
+
+// DList.empty()
+bool DList::empty() const
+{
+
+}
+
+// DList.insert()
+void DList::insert( ElementType item, int pos )
+{
+
+}
+
+// DList.erase()
+void DList::erase( int pos )
+{
+
+}
+
+// DList.display()
+void DList::display( std::ostream &out ) const
+{
+
+}
+
+// DList.getSize()
+int DList::getSize() const
+{
+
+}
+
+// DList.getCapacity()
+int DList::getCapacity() const
+{
+
+}
