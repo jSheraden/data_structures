5a3fd4f039e2c515ea8e7b46a7b9491d1f522e1c
jsheraden <josasheraden@gmail.com>
2016-03-19 03:17:31 +0000

    Started on constructors in hw4/DList.cpp

--- hw4/DList.cpp
+++ hw4/DList.cpp
@@ -1,6 +1,35 @@
 #include "DList.hpp"
 
+// The default constructor initializes all member variables.
 DList::DList()
 {
+    first = new Node;
+    mySize = 0;
+}
 
+// An overloaded constructor that assigns the values of a DList
+// object to a new one.
+DList::DList( const DList &source )
+{
+    this = source;
+}
+
+// The destructor deletes the dynamically allocated array.
+DList::~DList()
+{
+    delete[] first;
+}
+
+// Default constructor for the nested class Node.
+DList::Node::Node()
+{
+    data = 0;
+    next = NULL;
+}
+
+// Overloaded constructor for nested class Node.
+DList::Node::Node( ElementType initData )
+{
+    data = initData;
+    next = NULL;
 }
