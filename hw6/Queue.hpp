63aa0de121ff481e25f2d09b01c83545e464b217
jsheraden <josasheraden@gmail.com>
2016-04-09 05:11:05 +0000

    Finished hw6/Queue.hpp

---
+++ hw6/Queue.hpp
@@ -0,0 +1,30 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+class Queue
+{
+    private:
+        class Node
+        {
+            public:
+                QueueElement data;
+                Node *next;
+                Node( QueueElement value, Node *link = 0 ) : data( value ), next( link );
+        };
+        
+        typedef Node * NodePointer;
+        
+        NodePointer myFront, myBack;
+    public:
+        Queue();
+        Queue( const Queue &original );
+        ~Queue();
+        const Queue &operator=( const Queue &rightHandSide );
+        bool empty() const;
+        void enqueue( const QueueElement &value );
+        void display( std::ostream &out ) const;
+        QueueElement front() const;
+        void dequeue();
+};
+
+#endif
---
+++ hw6/main.cpp
@@ -0,0 +1,15 @@
+// Name: Joseph Sheraden
+// Project Number: 6
+// Project Description: Queue using a Circular Linked List
+// Course: Data Structures
+// Instructor: Bob Comer
+// Date: April 5, 2016
+#include <iostream>
+#include "Queue.cpp"
+
+int main()
+{
+    Queue queue;
+    std::cout << "Success!" << std::endl;
+    return 0;
+}
