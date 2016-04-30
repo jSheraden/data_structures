37df6331e0259ca498b8103653f3050aab60ae8d
Joseph <josasheraden@gmail.com>
2016-04-29 23:03:41 -0500

    Added assignment operator functionality to Stack class in hw5/Stack.cpp

--- hw5/Stack.cpp
+++ hw5/Stack.cpp
@@ -26,14 +26,56 @@ Stack::Stack( const Stack &original )
     }
 }
 
+// Definition of Stack destructor.
 Stack::~Stack()
 {
+    // Set pointers to run through the stack.
+    NodePointer currPtr = myTop; // Node to be deallocated.
+    NodePointer nextPtr;         // Its successor.
 
+    while ( currPtr != 0 )
+    {
+        nextPtr = currPtr->next;
+        delete currPtr;
+        currPtr = nextPtr;
+    }
 }
 
+// Definition of Stack assignment operator.
 const Stack &Stack::operator=( const Stack &rightHandSide )
 {
+    // Check that st != st.
+    if ( this != rightHandSide )
+    {
+        // Destroy current linked list.
+        this->~Stack();
 
+        // If the stack is empty.
+        if ( rightHandSide.empty() )
+        {
+            myTop = 0;
+        }
+
+        // If not, copy rightHandSide's list.
+        else
+        {
+            // Copy first node.
+            myTop = new Node( rightHandSide.top() );
+
+            // Set pointers to run through the stack's linked lists.
+            NodePointer lastPtr = myTop
+            NodePointer rhsPtr = rightHandSide.myTop->next;
+
+            while ( rhsPtr != 0 )
+            {
+                lastPtr->next = new Node( rhsPtr->data );
+                lastPtr = lastPtr->next;
+                rhsPtr = rhsPtr->next;
+            }
+        }
+    }
+
+    return *this;
 }
 
 bool Stack::empty() const
