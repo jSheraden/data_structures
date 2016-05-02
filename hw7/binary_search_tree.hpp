2819b0d614b7732e72ec0301188201e8effc9665
Joseph <josasheraden@gmail.com>
2016-05-02 01:53:54 -0500

    Added binary_search_tree.hpp and binary_search_tree.cpp

---
+++ hw7/binary_search_tree.hpp
@@ -0,0 +1,50 @@
+#ifndef BINARY_SEARCH_TREE_HPP
+#define BINARY_SEARCH_TREE_HPP
+
+#include <iostream>
+#include <new>
+
+template <typename DataType>
+
+class BST
+{
+    private:
+        class BinNode
+        {
+            public:
+                DataType data;
+                BinNode *left;
+                BinNode *right;
+
+                // BinNode constructors.
+                // Default - data part is the default DataType
+                // value, both links are null.
+                BinNode() : left( 0 ), right( 0 ) {}
+
+                // Explicit value - data part contains item,
+                // both parts are null.
+                BinNode( DataType item ) : data( item ), left( 0 ), right( 0 ) {}
+        };
+
+        // BinNode object pointer definition.
+        typedef BinNode * BinNodePointer;
+
+        // Private function members.
+        void search2( const DataType &item, bool &found, BinNodePointer &locPtr, BinNodePointer &parent ) const;
+        void inOrderAux( std::ostream &out, BinNodePointer subTreePtr ) const;
+        void graphAux( std::ostream &out, int indent, BinNodePointer subTreeRoot ) const;
+
+        // Private data members.
+        BinNodePointer myRoot;
+    public:
+        // Function members.
+        BST();
+        bool empty() const;
+        bool search( const DataType &item ) const;
+        void insert( const DataType &item );
+        void remove( const DataType &item );
+        void inOrder( std::ostream &out ) const;
+        void graph( std::ostream &out ) const;
+};
+
+#endif
--- hw7/main.cpp
+++ hw7/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <new>
+#include "binary_search_tree.hpp"
 
 int main()
 {
+    std::cout << "Success!" << std::endl;
     return 0;
 }
