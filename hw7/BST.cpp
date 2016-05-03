819562cb14cf335cd08b1e0a059df9d4e6231a1e
Joseph <josasheraden@gmail.com>
2016-05-03 03:35:49 -0500

    Added functionality for BST.insert() in hw7/BST.cpp

--- hw7/BST.cpp
+++ hw7/BST.cpp
@@ -20,20 +20,51 @@ bool BST<DataType>::search( const DataType &item ) const
 
     while ( !found && !locPtr = 0 )
     {
-        if ( item < locPtr->data )
+        if ( item < locPtr->data )      // Descend left.
             locPtr = locPtr->left;
-        else if ( locPtr->data < item )
+        else if ( locPtr->data < item ) // Descend right.
             locPtr = locPtr->right;
-        else
+        else                            // Item found.
             found = true;
     }
+
+    return found;
 }
 
 // BST.insert()
 template <typename DataType>
-inline void BST<DataType>::insert( const DataType &item )
+void BST<DataType>::insert( const DataType &item )
 {
+    BinNodePointer locPtr = myRoot; // Search pointer.
+    BinNodePointer parent = 0;      // Pointer to parent of current node.
+    bool found = false;             // Indicates of item is already in tree.
 
+    while ( !found && !locPtr == 0 )
+    {
+        parent = locPtr;
+
+        if ( item < locPtr->data )      // Descend left.
+            locPtr = locPtr->left;
+        else if ( locPtr->data < item ) // Descend right.
+            locPtr = locPtr->right;
+        else                            // Item found.
+            found = true;
+    }
+
+    if ( !found )
+    {
+        // Construct node containing item.
+        locPtr = new BinNode( item );
+
+        if ( parent == 0 )              // Empty tree.
+            myRoot = locPtr;
+        else if ( item < parent->data ) // Insert to left of parent.
+            parent->left = locPtr;
+        else                            // Insert to right of parent.
+            parent->right = locPtr;
+    }
+    else
+        std::cout << "Item already in tree." << std::endl;
 }
 
 // BST.remove()
