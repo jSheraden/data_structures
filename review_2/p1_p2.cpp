b90111758f619c5fc8c1efc098cf3fec90d5b690
Joseph <josasheraden@gmail.com>
2016-05-06 08:24:28 -0500

    Added review_2

---
+++ review_2/p1_p2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <new>
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+};
+
+int main()
+{
+    Node *p1, *p2;
+    p1 = new( nothrow ) Node;
+    p2 = new( nothrow ) Node;
+
+    p1->data = 12;
+    p2->data = 34;
+    p1 = p2;
+    p2->next = p1;
+
+    std::cout << p1->data << " " << p2->data << std::endl;
+    std::cout << p1->next->data << " " << p2->next->data << std::endl;
+
+    return 0;
+}
