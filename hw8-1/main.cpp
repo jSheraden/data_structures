881c9815a3803906a48c2be6104f30daa274d792
Joseph <josasheraden@gmail.com>
2016-05-08 08:51:49 -0500

    Added assignment header to hw8-1/main.cpp

--- hw8-1/main.cpp
+++ hw8-1/main.cpp
@@ -1,3 +1,9 @@
+// Name: Joseph Sheraden
+// Project Number: 8-1
+// Project Description: Hash Table with Linear Probing
+// Course: Data Structures
+// Instructor: Bob Comer
+// Date: May 8, 2016
 #include <iostream>
 #include "Table.hpp"
 
